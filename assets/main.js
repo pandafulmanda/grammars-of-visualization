80d414648cb0ff2fc29f46a849bc1af27f434c3a
pandafulmanda <pandafulmanda@gmail.com>
2016-09-12 17:39:38 -0500

    lots of updates

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,23 @@
 
 
 
+  window.addEventListener('keydown', function(keyEvent) {
+    var adjust = 1;
+    var previousKeys = ['ArrowLeft', 'ArrowUp', 'p'];
+
+    if(previousKeys.indexOf(keyEvent.key) > -1){
+      adjust = -1;
+    }
+
+    var nextY = Math.floor( window.scrollY / window.innerHeight + adjust) * window.innerHeight;
+
+    animatedScrollTo(
+      document.body, nextY, 200, function() {
+        console.log('done!')
+      }
+    );
+  });
+
 
 
 })(window);
---
+++ examples/ggplot-basic.R
@@ -0,0 +1,10 @@
+require(ggplot2)
+
+data_source <- "https://gist.githubusercontent.com/neerajt/6f0ff957e7ff3adab63471460cade5d5/raw/55e3b7691aefe8e1c1dca7543dd4d41c8b5bee09/members.json"
+
+members <- jsonlite::fromJSON(readLines(data_source))
+plt <- ggplot(members, aes(x=Date, y=TotalMembers)) +
+  geom_point() + geom_point(aes(y = ActiveMembers), color="red") + 
+  geom_point(aes(y = TotalMembers - ActiveMembers), color="blue")
+
+print(plt)
